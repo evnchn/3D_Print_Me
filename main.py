4fc89571bc518d6bb3a6a63e5b9268b6ba957879
evnchn <evanchan040511@gmail.com>
2024-12-24 14:00:32 +0800

    rudimentary job upload support

--- main.py
+++ main.py
@@ -15,6 +15,13 @@ import hmac
 import json
 import base64
 
+import uuid
+
+def generate_uuid():
+    return str(uuid.uuid4())
+
+from glob import glob
+
 # HMAC SHA256
 def hmac_sha256(msg, key=SECRET_KEY_BYTES):
     return hmac.new(key, msg, hashlib.sha256).hexdigest()
@@ -88,8 +95,8 @@ def main_page():
 
     def process_file(e):
         print(e) # UploadEventArguments(sender=<nicegui.elements.upload.Upload object at 0x00000214AAB4E270>, client=<nicegui.client.Client object at 0x00000214AAB4DD90>, content=<tempfile.SpooledTemporaryFile object at 0x00000214AA921AE0>, name='justcard.pdf', type='application/pdf')
-        # name is accessed by e.content.name
-        # type is accessed by e.content.type
+        # name is accessed by e.name
+        # type is accessed by e.type
         content = e.content
         print("Got file", content)
         
@@ -121,8 +128,119 @@ def main_page():
     ui.label("If you want to retain your session, copy this link (never expires, so keep it secret):")
     ui.button("Click and copy URL", on_click=copy_to_clipboard)
 
-    ui.label("If you want to ")
+    """ui.label("If you want to ")
     # button to navigate to the upload page
-    ui.button('Upload test', on_click=lambda: ui.navigate.to('/upload_test'))
+    ui.button('Upload test', on_click=lambda: ui.navigate.to('/upload_test'))"""
+
+    # button to navigate to the factories page
+    ui.button('Factories', on_click=lambda: ui.navigate.to('/show_factories'))
+
+
+@ui.page("/show_factories")
+def show_factories():
+    ui.label("Factories").classes('text-2xl')
+    ui.label("This page is restricted to authenticated users")
+
+    # list all the factories
+    ui.label("List of factories")
+
+    def new_job_to_factory(factory):
+        ui.navigate.to(f"/new_job_to_factory/{factory}")
+
+    def show_jobs_at_factory(factory):
+        ui.navigate.to(f"/show_jobs/{factory}")
+    
+    with ui.column().classes("w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3"):
+        # enumerate factories folder, for each subfolder, try open desc.json
+        for factory in glob("factories/*"):
+            basename = os.path.basename(factory)
+            with open(f"{factory}/desc.json", "r") as f:
+                desc = json.load(f)
+                with ui.card().classes("w-full"):
+                    ui.label(f"Factory: {desc['name']}").classes('text-xl')
+                    ui.markdown(desc['description'])
+                    ui.markdown(desc['upload_instructions'])
+                    ui.image(f"{factory}/{desc['cover_image']}").classes("w-full h-96").props("fit='contain'")
+                    ui.button("New Job", on_click=lambda factory=basename: new_job_to_factory(factory))
+                    ui.button("Show Jobs", on_click=lambda factory=basename: show_jobs_at_factory(factory))
+
+@ui.page("/new_job_to_factory/{factory}")
+def new_job_to_factory(factory: str):
+    # creates /jobs/{random UUID} folder
+    # writes the job_info.json file
+    my_uuid = generate_uuid()
+    os.makedirs(f"jobs/{my_uuid}")
+    with open(f"jobs/{my_uuid}/job_info.json", "w") as f:
+        json.dump({'factory': factory, 'status': 'new'}, f)
+
+    ui.navigate.to(f"/submit_to_factory/{my_uuid}")
+
+@ui.page("/submit_to_factory/{job_uuid}")
+def submit_to_factory(job_uuid: str):
+    # read the job_info.json file
+    with open(f"jobs/{job_uuid}/job_info.json", "r") as f:
+        job_info = json.load(f)
+        if job_info['status'] != 'new':
+            return ui.navigate.to("/")
+        factory = job_info['factory']
+        with open(f"factories/{factory}/desc.json", "r") as f:
+            desc = json.load(f)
+            ui.label(f"Factory: {desc['name']}").classes('text-2xl')
+            ui.markdown(desc['upload_instructions'])
+            ui.upload(on_upload=lambda e: process_file(e, job_uuid)).classes('max-w-full')
+
+    def process_file(e, job_uuid):
+        print(e)
+        content = e.content
+        print("Got file", content)
+
+        # save the content to a file
+        with open(f"jobs/{job_uuid}/{e.name}", "wb") as f:
+            f.write(content.read())
+
+        with open(f"jobs/{job_uuid}/job_info.json", "r") as f:
+            job_info = json.load(f)
+            job_info['status'] = 'submitted'
+            job_info['file'] = e.name
+            with open(f"jobs/{job_uuid}/job_info.json", "w") as f:
+                json.dump(job_info, f)
+
+        ui.navigate.to("/")
+
+@ui.page("/show_jobs/{factory}")
+def show_jobs(factory: str):
+    ui.label(f"Jobs at {factory}").classes('text-2xl')
+    with ui.column().classes("w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3"):
+        for job in glob(f"jobs/*"):
+            basename = os.path.basename(job)
+            with open(f"{job}/job_info.json", "r") as f:
+                job_info = json.load(f)
+                if job_info['factory'] == factory:
+                    with ui.card().classes("w-full"):
+                        ui.label(f"Job ID: {job}").classes('text-xl')
+                        ui.markdown(f"Status: {job_info['status']}")
+                        ui.button("View Job", on_click=lambda: ui.navigate.to(f"/show_job/{basename}"))
+
+@ui.page("/show_job/{job_uuid}")
+def show_job(job_uuid: str):
+    if not job_uuid:
+        return ui.navigate.to("/")
+
+    print("viewing job", job_uuid)
+    def download_job(job_uuid_2):
+        print("downloading job", job_uuid_2)
+        # use ui.download to download the job
+        with open(f"jobs/{job_uuid_2}/job_info.json", "r") as f:
+            job_info = json.load(f)
+            ui.download(f"jobs/{job_uuid_2}/{job_info['file']}")
+
+    with open(f"jobs/{job_uuid}/job_info.json", "r") as f:
+        job_info = json.load(f)
+        ui.label(f"Job ID: {job_uuid}").classes('text-2xl')
+        ui.markdown(f"Status: {job_info['status']}")
+
+    ui.button("Download Job", on_click=lambda job_uuid=job_uuid: download_job(job_uuid))
+
+
 
 ui.run(storage_secret=SECRET_KEY)
