2ffdcceea704a7f57f791b1df139ad35c41bc28f
evnchn <evanchan040511@gmail.com>
2025-01-08 13:40:41 +0800

    use the new auth defintion supporting master password defintion from env

--- auth_lib/security_definitions.py
+++ auth_lib/security_definitions.py
@@ -2,11 +2,24 @@ from fastapi import Request
 from fastapi.responses import RedirectResponse
 from nicegui import app
 from starlette.middleware.base import BaseHTTPMiddleware
+import os
+from dotenv import load_dotenv
 
 
-passwords = {'3dpm': '3dprintersarefun!', 'admin': 'topsecret'}
+passwords = {}
+load_dotenv()
+
+normal_pw = os.getenv('CREATE_ACCOUNT_NORMAL_PW')
+admin_pw = os.getenv('CREATE_ACCOUNT_ADMIN_PW')
+
+if normal_pw:
+    passwords['normal'] = normal_pw
+if admin_pw:
+    passwords['admin'] = admin_pw
 priviledged_users = {'admin'}
-unrestricted_page_routes = {'/login', '/reauth', '/register'}
+unrestricted_page_routes = {'/login', '/register'}
+
+admin_only_route_prefix = "/admin"
 
 
 class AuthMiddleware(BaseHTTPMiddleware):
@@ -16,7 +29,16 @@ class AuthMiddleware(BaseHTTPMiddleware):
     """
 
     async def dispatch(self, request: Request, call_next):
+        if request.url.path.startswith('/_nicegui') or request.url.path in unrestricted_page_routes or request.url.path.startswith('/endpoint'):
+            return await call_next(request)
+
         if not app.storage.user.get('authenticated', False):
-            if not request.url.path.startswith('/_nicegui') and request.url.path not in unrestricted_page_routes:
-                return RedirectResponse('/login')
+            return RedirectResponse('/login')
+        
+        if request.url.path.startswith(admin_only_route_prefix):
+            username = app.storage.user.get('username', '')
+            is_admin = app.storage.general.get("user_pw", {}).get(username, {}).get('admin', False)
+            if not is_admin:
+                return RedirectResponse('/')
+            
         return await call_next(request)
